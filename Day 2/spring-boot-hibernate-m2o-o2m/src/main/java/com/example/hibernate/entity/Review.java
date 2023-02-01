0ee6aebcaa54d17563acd658e30ae3065c527282
sam253narula <samarthnarula253@gmail.com>
2023-02-01 18:54:39 +0530

    first commit

---
+++ Day 2/spring-boot-hibernate-m2o-o2m/src/main/java/com/example/hibernate/entity/Review.java
@@ -0,0 +1,25 @@
+package com.example.hibernate.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Review {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private int rating;
+
+	private String description;
+	
+	@ManyToOne
+	private Course course;
+	
+}
